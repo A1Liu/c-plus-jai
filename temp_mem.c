40971a12fc623be336ef34d9572253a580cad6ec
Albert Liu <albertymliu@gmail.com>
2019-03-05 01:46:19 -0500

    Added true flexibility to va_format using vsnprintf

--- temp_mem.c
+++ temp_mem.c
@@ -9,8 +9,8 @@
 static void* alloc(size_t);
 static void mem_free(void);
 static void* root(void);
-static void* loc(void);
-static void set_loc(void* loc);
+static void* location(void);
+static void set_location(void* loc);
 static size_t size(void);
 static size_t capacity(void);
 static void set_capacity(size_t);
@@ -19,7 +19,7 @@ static char* va_format(char* fmt, va_list args);
 static void print(char* fmt, ...);
 static void println(char* fmt, ...);
 
-static char* location;
+static char* current_buffer_location;
 static char* mem_buffer = NULL;
 static int mem_buffer_capacity = DEFAULT_TMEM_CAPACITY;
 
@@ -27,12 +27,12 @@ static void*
 alloc(size_t size) {
   if (mem_buffer == NULL) {
     mem_buffer = malloc(sizeof(char*) * DEFAULT_TMEM_CAPACITY);
-    location = mem_buffer;
+    current_buffer_location = mem_buffer;
   }
-  void* current = location;
-  location += size / sizeof(char*);
+  void* current = current_buffer_location;
+  current_buffer_location += size / sizeof(char*);
   if (size % sizeof(char*) != 0)
-    location += 1;
+    current_buffer_location += 1;
   return current;
 }
 
@@ -42,7 +42,7 @@ mem_free(void)
 {
   free(mem_buffer);
   mem_buffer = NULL;
-  location = NULL;
+  current_buffer_location = NULL;
   mem_buffer_capacity = 0;
 }
 
@@ -55,24 +55,24 @@ root(void)
 
 static
 void*
-loc(void)
+location(void)
 {
-  return location;
+  return current_buffer_location;
 }
 
 static
 void
-set_loc(void* loc)
+set_location(void* loc)
 {
   if (loc == NULL) {
-    location = mem_buffer;
+    current_buffer_location = mem_buffer;
   } else if ((char*) loc < mem_buffer
       || (char*) loc > mem_buffer + capacity())
   { // If loc is outside our current buffer
-    location = mem_buffer = loc;
+    current_buffer_location = mem_buffer = loc;
     mem_buffer_capacity = 0;
   } else {
-    location = loc;
+    current_buffer_location = loc;
   }
 }
 
@@ -80,7 +80,7 @@ static
 size_t
 size(void)
 {
-  return (location - mem_buffer) * sizeof(char*);
+  return (current_buffer_location - mem_buffer) * sizeof(char*);
 }
 
 static
@@ -97,15 +97,13 @@ set_capacity(size_t size)
   if (mem_buffer_capacity == 0) {
     mem_buffer_capacity = size / sizeof(char*);
   } else if (size <= capacity() && size > capacity() * .75 ) {
-    location = mem_buffer;
+    current_buffer_location = mem_buffer;
   } else {
     free(mem_buffer);
-    location = mem_buffer = malloc(size);
+    current_buffer_location = mem_buffer = malloc(size);
     mem_buffer_capacity = size / sizeof(char*);
   }
 }
-
-static char* str_buffer;
 
 static
 char*
@@ -122,12 +120,34 @@ static
 char*
 va_format(char* fmt, va_list args)
 {
-  if (str_buffer == NULL)
-    str_buffer = malloc(sizeof(char) * 1000);
-  vsprintf(str_buffer, fmt, args);
-  char* out = alloc(sizeof(char) * (strlen(str_buffer) + 1));
-  strcpy(out, str_buffer);
-  return out;
+  // Weights that determine learning rate
+  static const float NEW_WEIGHT = 1 / 4.0;
+  static const float OLD_WEIGHT = 1 - NEW_WEIGHT;
+
+  // Prediction for difference in output and format length
+  static int prediction = 5;
+
+  // temporary vars
+  static int fmt_len, predicted_len, len, actual;
+
+  // Get data about format string
+  fmt_len = strlen(fmt);
+  predicted_len = fmt_len + prediction;
+
+  // Create and use buffer w/ predicted length
+  char* buffer = alloc(sizeof(char) * predicted_len);
+  len = vsnprintf(buffer, predicted_len + 1, fmt, args);
+  actual = len - fmt_len;
+  prediction = actual * NEW_WEIGHT + prediction * OLD_WEIGHT;
+  if (len <= predicted_len) {
+    // Set location back to not waste space
+    set_location(buffer - (predicted_len - len) * sizeof(char) / sizeof(char*));
+    return buffer;
+  } else {
+    alloc(sizeof(char) * (len - predicted_len));
+    vsprintf(buffer, fmt, args);
+    return buffer;
+  }
 }
 
 static
@@ -152,7 +172,7 @@ println(char* fmt, ...)
 
 const TemporaryMemory TEMPORARY_MEMORY_NAMESPACE = {
   alloc, mem_free, root,
-  loc, set_loc,
+  location, set_location,
   size, capacity, set_capacity };
 const TemporaryString TEMPORARY_STRING_NAMESPACE = {format,va_format,print,println};
 
