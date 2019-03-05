387b8886687da26150eaa73108749e8b197793fc
Albert Liu <albertymliu@gmail.com>
2019-03-05 10:18:27 -0500

    added debug mode using preprocessor, fixed problems

--- temp_mem/temp_mem.c
+++ temp_mem/temp_mem.c
@@ -1,4 +1,3 @@
-#define TEMPORARY_MEMORY_NAMESPACE T
 #include "temp_mem.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,6 +5,10 @@
 
 #ifndef DEFAULT_TMEM_CAPACITY
 #define DEFAULT_TMEM_CAPACITY 10000
+#endif
+
+#ifndef TEMPORARY_MEMORY_DEBUG_MESSAGES
+#define TEMPORARY_MEMORY_DEBUG_MESSAGES false
 #endif
 
 static void* alloc(size_t);
@@ -24,14 +27,15 @@ static void println(char* fmt, ...);
 
 static char* current_buffer_location;
 static char* mem_buffer = NULL;
-static int mem_buffer_capacity = DEFAULT_TMEM_CAPACITY;
+static uint32_t mem_buffer_capacity = DEFAULT_TMEM_CAPACITY;
 
 const TemporaryMemory TEMPORARY_MEMORY_NAMESPACE = {
   alloc, mem_free, root, change_buffer,
   location, set_location,
   size, capacity, set_capacity };
 
-const TemporaryString TEMPORARY_STRING_NAMESPACE = {format,va_format,print,println};
+const TemporaryString TEMPORARY_STRING_NAMESPACE =
+  { format, va_format, print, println };
 
 const TemporaryMemoryUtilities TEMPORARY_MEMORY_UTILITIES_NAMESPACE = {
   alloc, mem_free, root, change_buffer,
@@ -43,13 +47,22 @@ static void*
 alloc(size_t size) {
   static void* current;
   if (mem_buffer == NULL) {
-    mem_buffer = malloc(sizeof(char*) * DEFAULT_TMEM_CAPACITY);
-    current_buffer_location = mem_buffer;
+    #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+    printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+        ".alloc(): Buffer allocation of size %d\n",
+        DEFAULT_TMEM_CAPACITY * sizeof(char*));
+    #endif
+    set_capacity(DEFAULT_TMEM_CAPACITY * sizeof(char*));
   }
   current = current_buffer_location;
   current_buffer_location += size / sizeof(char*);
   if (size % sizeof(char*) != 0)
     current_buffer_location += 1;
+  #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+  if (current_buffer_location > mem_buffer + mem_buffer_capacity)
+    printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+        ".alloc(): Buffer overflow on next call\n");
+  #endif
   return current;
 }
 
@@ -57,6 +70,10 @@ static
 void
 mem_free(void)
 {
+  #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+  printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+      ".free(): Freeing buffer at %p\n", mem_buffer);
+  #endif
   free(mem_buffer);
   mem_buffer = NULL;
   current_buffer_location = NULL;
@@ -74,6 +91,10 @@ static
 void
 change_buffer(void* new_root, size_t new_capacity)
 {
+  #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+  printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+      ".change_buffer(): Moving to %p\n", new_root);
+  #endif
   mem_free();
   mem_buffer = new_root;
   set_capacity(new_capacity);
@@ -91,8 +112,17 @@ void
 set_location(void* loc)
 {
   if (loc == NULL) {
+    #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+    printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+        ".set_loc(): Moving location back to root at %p\n", mem_buffer);
+    #endif
     current_buffer_location = mem_buffer;
   } else {
+    #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+    printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+        ".set_loc(): Moving to %p, bounded by (%p, %p]\n",
+        loc, mem_buffer, mem_buffer + mem_buffer_capacity);
+    #endif
     current_buffer_location = loc;
   }
 }
@@ -115,9 +145,14 @@ static
 void
 set_capacity(size_t size)
 {
-  static size_t true_capacity = DEFAULT_TMEM_CAPACITY;
+  static size_t true_capacity;
   if (mem_buffer_capacity == 0) {
+    #if TEMPORARY_MEMORY_DEBUG_MESSAGES
+    printf("[DEBUG] " #TEMPORARY_MEMORY_NAMESPACE
+        ".set_capacity(): Setting capacity to %d\n", size);
+    #endif
     mem_buffer_capacity = size / sizeof(char*);
+    true_capacity = mem_buffer_capacity * sizeof(char*);
   } else if (size <= true_capacity && size > true_capacity * .75 ) {
     mem_buffer_capacity = size / sizeof(char*);
     current_buffer_location = mem_buffer;
@@ -125,7 +160,8 @@ set_capacity(size_t size)
     if (mem_buffer != NULL)
       free(mem_buffer);
     current_buffer_location = mem_buffer = malloc(size);
-    true_capacity = mem_buffer_capacity = size / sizeof(char*);
+    mem_buffer_capacity = size / sizeof(char*);
+    true_capacity = mem_buffer_capacity * sizeof(char*);
   }
 }
 
@@ -144,32 +180,21 @@ static
 char*
 va_format(char* fmt, va_list args)
 {
-  // Weights that determine learning rate
-  static const float NEW_WEIGHT = 1 / 4.0;
-  static const float OLD_WEIGHT = 1 - NEW_WEIGHT;
-
-  // Prediction for difference in output and format length
-  static int prediction = 5;
-
-  // temporary vars
-  static int fmt_len, predicted_len, len, actual;
-
-  // Get data about format string
-  fmt_len = strlen(fmt);
-  predicted_len = fmt_len + prediction;
-
+  static const int predicted_len = 25;
   // Create and use buffer w/ predicted length
   char* buffer = alloc(sizeof(char) * predicted_len);
-  len = vsnprintf(buffer, predicted_len + 1, fmt, args);
-  actual = len - fmt_len;
-  prediction = actual * NEW_WEIGHT + prediction * OLD_WEIGHT;
-  if (len <= predicted_len) {
+  int len = vsnprintf(buffer, predicted_len + 1, fmt, args);
+  if (len == predicted_len) {
+    return buffer;
+  } else if (len < predicted_len) {
     // Set location back to not waste space
-    set_location(
-        buffer - (predicted_len - len) * sizeof(char) / sizeof(char*));
+    set_location(buffer);
+    alloc(len * sizeof(char*));
     return buffer;
   } else {
-    alloc(sizeof(char) * (len - predicted_len));
+    size_t size_diff = (len - predicted_len) * sizeof(char);
+    size_diff = size_diff - len * sizeof(char) % sizeof(char*);
+    alloc(size_diff); // Can waste up to 14 bytes
     vsprintf(buffer, fmt, args);
     return buffer;
   }
@@ -179,19 +204,23 @@ static
 void
 print(char* fmt, ...)
 {
+  void* loc = location();
   va_list args;
   va_start(args, fmt);
   printf("%s",va_format(fmt, args));
   va_end(args);
+  set_location(loc);
 }
 
 static
 void
 println(char* fmt, ...)
 {
+  void* loc = location();
   va_list args;
   va_start(args, fmt);
   printf("%s\n",va_format(fmt, args));
   va_end(args);
+  set_location(loc);
 }
 
